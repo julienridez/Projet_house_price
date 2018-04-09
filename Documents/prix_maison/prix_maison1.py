b6b27fa5bcb99092c37e400bb0ae9568e82809d1
julienridez <julienridez@yahoo.fr>
2018-04-09 12:24:26 +0200

    prix_maison1.py

--- Documents/prix_maison/prix_maison1.py
+++ Documents/prix_maison/prix_maison1.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
 import seaborn as sns
+import numpy as np
+from scipy.stats import norm
+from sklearn.preprocessing import StandardScaler
+from scipy import stats
+import warnings
+warnings.filterwarnings('ignore')
+#%matplotlib inline
 
 df = pd.read_csv('test.csv')
 train = pd.read_csv('train.csv')
@@ -20,6 +26,39 @@ y = 'OverallQual'
 a = 'GrLivArea'
 
 
-plt.figure()
-sns.barplot(a,y,data=df)
-sns.barplot(a,y,data=df)
+#plt.figure()
+#sns.barplot(a,y,data=df)
+#sns.barplot(a,y,data=df)
+sns.distplot(train['SalePrice']);
+print("Skewness: %f" % train['SalePrice'].skew())
+print("Kurtosis: %f" % train['SalePrice'].kurt())
+
+train['SalePrice'].describe()
+
+#scatter plot grlivarea/saleprice
+var = 'GrLivArea'
+data = pd.concat([train['SalePrice'], train[var]], axis=1)
+
+#scatter plot totalbsmtsf/saleprice
+var = 'TotalBsmtSF'
+data = pd.concat([train['SalePrice'], train[var]], axis=1)
+data.plot.scatter(x=var, y='SalePrice', ylim=(0,800000));
+
+#box plot overallqual/saleprice
+var = 'OverallQual'
+data = pd.concat([train['SalePrice'], train[var]], axis=1)
+f, ax = plt.subplots(figsize=(8, 6))
+fig = sns.boxplot(x=var, y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000);
+
+var = 'YearBuilt'
+data = pd.concat([train['SalePrice'], train[var]], axis=1)
+f, ax = plt.subplots(figsize=(16, 8))
+fig = sns.boxplot(x=var, y="SalePrice", data=data)
+fig.axis(ymin=0, ymax=800000);
+plt.xticks(rotation=90);
+
+#correlation matrix
+corrmat = train.corr()
+f, ax = plt.subplots(figsize=(12, 9))
+sns.heatmap(corrmat, vmax=.8, square=True);
